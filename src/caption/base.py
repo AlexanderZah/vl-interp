179344b2601c21bdc77e255e06bc74bf5b8d9b6f
AleksaAleksZakharov <AleksaAleksZakharov@Greenatom.ru>
2025-06-21 00:16:49 +0300

    3

--- src/caption/base.py
+++ src/caption/base.py
@@ -5,7 +5,7 @@ import logging
 from PIL import Image
 import torch
 import string
-from torchvision import transforms
+# from torchvision import transforms
 import numpy as np
 from scipy.stats import entropy
 
