4adb5c31aef1b1e24c39fa61fd67abeb3953b663
AleksaAleksZakharov <AleksaAleksZakharov@Greenatom.ru>
2025-06-21 16:40:18 +0300

    3

--- methods/algorithms.py
+++ methods/algorithms.py
@@ -109,7 +109,7 @@ def internal_confidence_heatmap(tokenizer, softmax_probs, class_):
 
 
 def internal_confidence_segmentation(tokenizer, softmax_probs, class_, num_patches=24):
-    class_token_indices = tokenizer.encode(class_)[1:]
+    class_token_indices = tokenizer.encode(class_)[0:]
     return (
         softmax_probs[class_token_indices]
         .max(axis=0)
