922f7beecd89f3954c4a5e9aa43c0222702c9082
AleksaAleksZakharov <AleksaAleksZakharov@Greenatom.ru>
2025-06-22 01:07:57 +0300

    3

--- methods/internvl_utils.py
+++ methods/internvl_utils.py
@@ -352,7 +352,7 @@ def retrieve_logit_lens_internvl(state, img_path, text_prompt=None, num_patches=
         raise ValueError(f"Expected {num_image_tokens} image tokens, found {len(image_token_indices)}.")
 
     # Обработка скрытых состояний
-    hidden_states_per_layer = output.hidden_states[1:]  # Пропускаем входной (embedding)
+    hidden_states_per_layer = output.hidden_states[1]  # Пропускаем входной (embedding)
     hidden_states = torch.stack(hidden_states_per_layer)  # (num_layers, batch, seq_len, hidden_size)
     print(f"Initial hidden_states shape: {hidden_states.shape}")
     print(f"Initial hidden_states sample (layer 0, batch 0, first 5 tokens): {hidden_states[0, 0, :5]}")
