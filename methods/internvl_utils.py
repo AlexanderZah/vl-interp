22a7a4435e22b14e1f167f8b72b8b51078cd365f
AleksaAleksZakharov <AleksaAleksZakharov@Greenatom.ru>
2025-06-21 15:17:34 +0300

    3

--- methods/internvl_utils.py
+++ methods/internvl_utils.py
@@ -286,7 +286,7 @@ def retrieve_logit_lens_internvl(state, img_path, text_prompt=None, num_patches=
             logit_scores = logits_warper(input_ids, logit_scores_processed)
             softmax_probs_layer = torch.nn.functional.softmax(logit_scores, dim=-1)
             softmax_probs_layer = softmax_probs_layer[:, image_token_index:image_token_index + num_image_tokens]
-            softmax_probs.append(softmax_probs_layer.cpu().numpy())
+            softmax_probs.append(softmax_probs_layer.to(torch.float16).cpu().numpy())
         del curr_layer_logits, logit_scores, logit_scores_processed, softmax_probs_layer
     
     softmax_probs = np.stack(softmax_probs).transpose(2, 0, 1)
