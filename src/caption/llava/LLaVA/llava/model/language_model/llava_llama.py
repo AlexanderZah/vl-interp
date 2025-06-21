a8217f8d9817bae4c0ea903eb1d7da115e2a34dd
AleksaAleksZakharov <AleksaAleksZakharov@Greenatom.ru>
2025-06-21 03:34:13 +0300

    3

--- src/caption/llava/LLaVA/llava/model/language_model/llava_llama.py
+++ src/caption/llava/LLaVA/llava/model/language_model/llava_llama.py
@@ -134,8 +134,8 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
             )
         else:
             inputs_embeds = self.get_model().embed_tokens(inputs)
-        del kwargs['cache_position']
-        print(kwargs.keys())
+
+
         return super().generate(
             position_ids=position_ids,
             attention_mask=attention_mask,
